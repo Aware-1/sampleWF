eb6fcec4342725b8d4e92805e0daad0e8057c1ca
ali <ali81esl12@gamil.com>
2024-10-21 14:42:54 +0330

    112

--- WebApplication2/Data/Model/Order.cs
+++ WebApplication2/Data/Model/Order.cs
@@ -1,7 +1,17 @@
-﻿namespace WebApplication2.Data.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Data.Model;
 
 public class Order
 {
     public int Id { get; set; }
     
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public int UserId { get; set; }
+    
+    public bool IsFinally { get; set; }
+
+    
+    public ICollection<OrderDetail> OrderDetails { get; set; }
+
 }
--- WebApplication2/Data/Model/Product.cs
+++ WebApplication2/Data/Model/Product.cs
@@ -1,7 +1,25 @@
 ﻿namespace WebApplication2.Data.Model;
+using System.ComponentModel.DataAnnotations;
 
 public class Product
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public int UserId { get; set; }
+    
+    [Display(Name = "نام محصول")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(500, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string Name { get; set; }
+    
+    [Display(Name = "قیمت محصول")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    public int Price { get; set; }
+    
+    
+    
+    
+    public ICollection<OrderDetail> OrderDetails { get; set; }
 
 }
